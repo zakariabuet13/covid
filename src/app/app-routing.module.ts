aadf4293aff500ed115d5b5a19204179e0dc1915
Zakaria Ahmed <zakaria.buet13@gmail.com>
2022-01-30 14:20:42 +0600

    Remove login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,9 @@ import { AuthGuard } from './auth.guard';
 import { ArchiveComponent } from './archive/archive.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
   // { path: 'history', component: ArchiveComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
